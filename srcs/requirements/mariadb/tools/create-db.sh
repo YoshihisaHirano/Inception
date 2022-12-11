18d7c2c8a9cca446f3ac53ebe81d056d121afb8b
Anelle Alannys <anastasia7febr@gmail.com>
2022-12-11 18:28:46 +0400

    added nginx container; server responds with database connection error

--- srcs/requirements/mariadb/tools/create-db.sh
+++ srcs/requirements/mariadb/tools/create-db.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-
-apt-get update
-apt-get install -y mariadb-server
+userdel mysql
+#makes sure that the correct user no. owns mysql directory
+useradd -u 1001 mysql
+chown -R mysql:mysql /var/lib/mysql
 
 service mysql start
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;\
-DROP USER IF EXISTS '$MYSQL_USER'@'%';\
-CREATE USER "$MYSQL_USER"@"%" IDENTIFIED BY '$MYSQL_PASSWORD';\
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';\
-FLUSH PRIVILEGES;"
-mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
+mysql -u root -e "DROP DATABASE IF EXISTS $MYSQL_DATABASE"
+mysql -u root -e "DROP USER IF EXISTS '$MYSQL_USER'@'%'"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION"
+mysql -u root -e "FLUSH PRIVILEGES"
+mysqladmin -u root password $MYSQL_ROOT_PASSWORD
